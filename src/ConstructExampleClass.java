bc5846a43f558ae5b5ec273b0d195a3b7004bce1
LAWRENCE PINTO <lpinto1204@gmail.com>
2022-06-28 11:02:41 +0530

    changes

--- src/ConstructExampleClass.java
+++ src/ConstructExampleClass.java
@@ -3,15 +3,24 @@ public class ConstructExampleClass {
 
         Pup p = new Pup("Kramer", 3);
         System.out.println(p);
+        Pup p1 = new Pup("Jarome", 8, "Dallas");
+        System.out.println(p1);
     }
 
     public static class Pup {
         String name;
         int age;
+        String location;
 
         public Pup(String name, int age ) {
             this.name = name;
             this.age = age;
+        }
+
+        public Pup(String name, int age, String location ) {
+            this.name = name;
+            this.age = age;
+            this.location = location;
         }
 
         @Override
@@ -19,6 +28,7 @@ public class ConstructExampleClass {
             return "Pup{" +
                     "name='" + name + '\'' +
                     ", age=" + age +
+                    ", location='" + location + '\'' +
                     '}';
         }
     }
