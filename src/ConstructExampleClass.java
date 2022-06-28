a2389eb0ddb4b90f686dfed9714ebb032a6429b0
LAWRENCE PINTO <lpinto1204@gmail.com>
2022-06-28 10:56:06 +0530

    constructor example

--- src/ConstructExampleClass.java
+++ src/ConstructExampleClass.java
@@ -1,5 +1,25 @@
 public class ConstructExampleClass {
     public static void main(String[] args) {
 
+        Pup p = new Pup("Kramer", 3);
+        System.out.println(p);
+    }
+
+    public static class Pup {
+        String name;
+        int age;
+
+        public Pup(String name, int age ) {
+            this.name = name;
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return "Pup{" +
+                    "name='" + name + '\'' +
+                    ", age=" + age +
+                    '}';
+        }
     }
 }
